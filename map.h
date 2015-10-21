d3c6de9a0839bb9b7fd950170632f55b89918820
chunyenho <chunyenher@gmail.com>
2015-10-21 18:40:49 +0800

    Complete move and pull function

--- map.h
+++ map.h
@@ -1,5 +1,6 @@
 char map[5][4];
 typedef char bool;
+int steps = 0;
 /*
 人名    代碼	大小	個數
 -------------------------------
@@ -14,12 +15,15 @@ typedef char bool;
 enum {Free, TT, GY, CF, MC, ZY, HC, Z};
 enum {UP, RIGHT, DOWN, LEFT};
 void init(void);
+void pull(char charac);
 void push(char charac, int x, int y);
-bool move(char dir);
+bool move(char* dir);
 void status(void);
 void print_name(int x, int y);
-int findfree();
-bool checkcomplete(); 
+int find_free();
+int find_charac(char charac, int* x, int* y);
+bool can_move(char charac, char dir, int x, int y);
+bool check_complete(); 
 //Initialize map to specific mode
 //Set 0 to count
 void init()
@@ -42,6 +46,21 @@ void init()
     push(Z,3,2);
     push(Z,4,0);
     push(Z,4,3);
+}
+// pull character out from map
+void pull(char charac)
+{
+    int i, j;
+    for (i=0; i<5; i++)
+    {
+        for (j=0; j<4; j++)
+        {
+            if(map[i][j] == charac)
+            {
+                map[i][j] = 0;
+            }
+        }
+    }
 }
 // push character into map[x][y]
 void push(char charac, int x, int y)
@@ -119,11 +138,135 @@ void push(char charac, int x, int y)
 
 }
 //Move fuction
-//Input:    UP, RIGHT, DOWN, LEFT
+//Input:    FU(First Up), FR, FD, FL, SU(Second Up), SR, SD, SL
 //Output:   0(fail), 1(success) 
-bool move(char dir);
+bool move(char* dir);
 {
+    char succ = 0;
+    int fir_x, fir_y, sec_x, sec_y, tmp, tmp_x, tmp_y;
+    tmp = findfree();
+    fir_x = (tmp>>24) & 0xff;
+    fir_y = (tmp>>16) & 0xff;
+    sec_x = (tmp>>8 ) & 0xff;
+    sec_y =  tmp      & 0xff;
+    if(dir[0]=='F')
+    {
+        switch(dir[1])
+        {
+            case 'U':
+                if(map[fir_x][fir_y+1])
+                {
+                    tmp = map[fir_x][fir_y+1]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,U,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x,tmp_y-1);
+                    }    
+                }
+                break;
+            case 'R':
+                if(map[fir_x-1][fir_y])
+                {
+                    tmp = map[fir_x-1][fir_y]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,R,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x+1,tmp_y);
+                    }    
+                }
+                break;
+            case 'D':
+                if(map[fir_x][fir_y-1])
+                {
+                    tmp = map[fir_x][fir_y-1]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,D,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x,tmp_y+1);
+                    }    
+                }
+                break;
+            case 'L':
+                if(map[fir_x+1][fir_y])
+                {
+                    tmp = map[fir_x+1][fir_y]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,L,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x-1,tmp_y);
+                    }    
+                }
+                break;
+        }
+    }
+    if(dir[0]=='S')
+    {
+        switch(dir[1])
+        {
+            case 'U':
+                if(map[sec_x][sec_y+1])
+                {
+                    tmp = map[sec_x][sec_y+1]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,U,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x,tmp_y-1);
+                    }    
+                }
+                break;
+            case 'R':
+                if(map[sec_x-1][sec_y])
+                {
+                    tmp = map[sec_x-1][sec_y]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,R,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x+1,tmp_y);
+                    }    
+                }
+                break;
+            case 'D':
+                if(map[sec_x][sec_y-1])
+                {
+                    tmp = map[sec_x][sec_y-1]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,D,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x,tmp_y+1);
+                    }    
+                }
+                break;
+            case 'L':
+                if(map[sec_x+1][sec_y])
+                {
+                    tmp = map[sec_x+1][sec_y]; //tmp record the character
+                    find_charac((char)tmp,&tmp_x,&tmp_y);
+                    if(can_move((char)tmp,L,tmp_x,tmp_y))
+                    {
+                        steps++;
+                        pull((char)tmp);
+                        push((char)tmp,tmp_x-1,tmp_y);
+                    }    
+                }
+                break;
+        }
+    }
 }
+
 //See map status, and print out
 void status()
 {
@@ -172,7 +315,7 @@ void print_name(int x, int y)
 //Output: two x-y coordinates
 //        (32bit int 
 //        0x X1X1 Y1Y1 X2X2 Y2Y2)
-int findfree()
+int find_free()
 {
     int count = 0, result = 0;
     for(i=0; i<5; i++)
@@ -191,7 +334,7 @@ int findfree()
     }
 }
 //Check map whether it's completed
-bool checkcomplete()
+bool check_complete()
 {
     if(map[3][1]==TT && map[3][2]==TT && map[4][1]==TT && map[4][2]==TT)
     {
