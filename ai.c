a6f7f7e8bbafa3715dae81e68e674b13221fa30c
chunyenho <chunyenher@gmail.com>
2015-10-29 02:02:35 +0800

    Ver1 can compile but segamentation fault

--- ai.c
+++ ai.c
@@ -4,46 +4,51 @@
    some searching skills.
    2015/10/23
  */
+#include <stdlib.h>
+#include <stdio.h>
 #include "map.h" 
 typedef struct node 
 {
     char map[5][4];
-    bool visited;
-    node* parent;
-    node* FU;
-    node* FR;
-    node* FD;
-    node* FL;
-    node* SU;
-    node* SR;
-    node* SD;
-    node* SL;
+    struct node* parent;
+    struct node* FU;
+    struct node* FR;
+    struct node* FD;
+    struct node* FL;
+    struct node* SU;
+    struct node* SR;
+    struct node* SD;
+    struct node* SL;
 }node;
 
 typedef struct queue
 {
     node* now;
-    queue* next;
-    queue* prev;
+    struct queue* next;
+    struct queue* prev;
 }queue;
 
 typedef struct map_data
 {
     char map_data[5][4];
-    map_data* next;
+    struct map_data* next;
 }map_data;
+
+queue* head,* tail;
+map_data* head_data = 0;
+map_data* database_arr[256] = {0};
 /* function in map.h */
 bool check_complete();
 
 void search_childs (node* parent);
-bool create_node (node*  parent, char* dir);
-void path_to_root (node);
+node* create_node (node*  parent, char* dir);
+//void path_to_root (node);
 void enqueue(node* now);
 void dequeue();
-void store_to_database(node* now)
+bool search_store_to_database(node* now);
 bool search_database(node* now);
 char transfer_map(char data);
-long long node_count = 0; /*  node_count: record the total number of nodes, and it will be added by create_node. */
+int node_count = 0; /*  node_count: record the total number of nodes, and it will be added by create_node. */
 
 /*
 char transfer_map(char data):
@@ -51,7 +56,7 @@ char transfer_map(char data):
 */
 char transfer_map(char data)
 {
-    switch(map[x][y])
+    switch(data)
     {
         case 0:
             return 0;
@@ -70,7 +75,7 @@ char transfer_map(char data)
         case H:
             return Z;
     }
-
+    return 0;
 }
 /*
 dequeue:
@@ -82,8 +87,10 @@ void dequeue()
         printf("Dequeue error!!");
     else
     {
-        search_child(tail);
-        node* tmp = tail;
+        search_childs(tail->now);
+        queue* tmp = tail;
+        node* tmp_n = tail->now;
+        //queue have 2up nodes
         if(tail->prev)
         {
             tail->prev->next = NULL;
@@ -92,9 +99,9 @@ void dequeue()
         else
         {
             head = 0;
-            free(tail);
             tail = 0;
         }
+        free(tmp_n);
         free(tmp);
     }      
 }
@@ -104,20 +111,23 @@ enqueue:
 */
 void enqueue(node* now)
 {
+    node_count++;
+    queue* new = (queue *)malloc(sizeof(queue));
+    new->now = now;
     if(!head && !tail) //first node
     {
-        head = now;
-        tail = now;
-        now->next = 0;
-        now->prev = 0;
+        head = new;
+        tail = new;
+        new->next = 0;
+        new->prev = 0;
         return;
     }
     else // Add to head
     {  
-        now->next = head;
-        now->prev = 0;
-        head->prev = now;
-        head = now;
+        new->next = head;
+        new->prev = 0;
+        head->prev = new;
+        head = new;
         return;
     }
 }
@@ -127,8 +137,6 @@ This function call create_node 8 times for building childs.
  */
 void search_childs (node* parent)
 {
-    if(!parent->visited)
-    {
         parent->FU = create_node(parent, "FU");
         parent->FR = create_node(parent, "FR");
         parent->FD = create_node(parent, "FD"); 
@@ -137,10 +145,6 @@ void search_childs (node* parent)
         parent->SR = create_node(parent, "SR");
         parent->SD = create_node(parent, "SD");
         parent->SL = create_node(parent, "SL");
-        parent->visited = 1;
-    }
-    else
-        printf("node visited error!!");
 }
 /*
    bool create_node():
@@ -158,6 +162,7 @@ node* create_node (node*  parent, char* dir)
         }
     }
     node* child = (node*)malloc(sizeof(node));
+    child->parent = parent;
     if(move(dir))
     {
         for(i=0; i<5; i++)
@@ -167,7 +172,7 @@ node* create_node (node*  parent, char* dir)
                 child->map[i][j] = map[i][j];
             }   
         }
-        if(!search_store_to_database(child))
+       if(!search_store_to_database(child))
         {
             enqueue(child);
             check_complete();
@@ -181,14 +186,14 @@ node* create_node (node*  parent, char* dir)
 char path_to_root(node):
    return a char array for finding the answer node.
  */
-char* path_to_root(node);
+//char* path_to_root(node);
 /*
 void store_to_database(node* now):
     This function store map[5][4] Info to database. If find return 1, else return 0.
 */
 bool search_store_to_database(node* now)
 {
-    char parity = 0;
+/*    char parity = 0;
     map_data* new_data = (map_data *)malloc(sizeof(map_data));
     char i,j;
     map_data* tmp ;
@@ -205,12 +210,12 @@ bool search_store_to_database(node* now)
     {   
         for(j=0;j<4;j++)
         {   
-            parity ^= new_data->map_data[i][j];
+            parity += new_data->map_data[i][j];
         }   
     }
     //find and store the node 
     tmp = database_arr[parity];
-    
+    char fail;
     while(tmp)
     {
         fail = 0;
@@ -237,28 +242,64 @@ bool search_store_to_database(node* now)
     node_count++;
     new_data->next = database_arr[parity];
     database_arr[parity] = new_data;
+    return 0;*/
+    
+    /* Another way */
+    
+    int fail,i,j;
+    map_data* new_data = (map_data *)malloc(sizeof(map_data));
+    map_data* tmp;
+    for(i=0; i<5; i++)
+    {   
+        for(j=0;j<4;j++)
+        {
+            new_data->map_data[i][j] = transfer_map(now->map[i][j]);
+        }
+    }
+    tmp = head_data;
+    while(tmp)
+    {
+        fail = 0;
+        for(i=0; i<5; i++)
+        {
+            for(j=0;j<4;j++)
+            {
+                if(tmp->map_data[i][j] != new_data->map_data[i][j])
+                {
+                    i=5;
+                    j=4;
+                    fail = 1;
+                }
+            }
+        }
+        if(!fail)
+        {
+            free(new_data);
+            return 1;
+        }
+        tmp = tmp->next;
+    }
+    new_data->next = head_data; 
+    head_data = new_data;
     return 0;
 }
-queue* head,* tail;
-map_data* database_arr[256] = {0};
 
 int main()
 {    
     // Set the BFS tree root
     node* root = (node *)malloc(sizeof(node)); /* node root:    root node(original map state). */
     root->parent = 0;
-    root->visited = 0;
     init();
+    status();
     int i, j;
     for(i=0; i<5; i++)
         for(j=0; j<4; j++)
             root->map[i][j] = map[i][j];
-    queue* first_node = (queue *)malloc(sizeof(queue));
-    first_node->now = root;
-    enqueue(first_node);
+    enqueue(root);
     while(1)
     {
         dequeue();
+        printf("node count: %d",node_count);
     }
     return 0;
 }
